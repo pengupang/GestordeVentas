6f5e4ddeed1fa0a230937bc66923fa2d4ccb8a29
FabianPeralta08 <fabian.peralta07@gmail.cl>
2024-10-30 23:12:31 -0300

    Update views.py
    Mensajes de ingreso de empleados y/o proveedores

--- App/views.py
+++ App/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
 from App.models import Proveedor, Empleado
 from .forms import ProveedorForm, EmpleadoForm
 
@@ -23,6 +24,10 @@ def agregar_empleado(request):
         form = EmpleadoForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Empleado Ingresado')
+            return redirect('empleados_agregar/') 
+        else:
+            messages.error(request, 'Error empleado no ingresado')
     data = {'form': form }
     return render(request,'empleados_agregar.html',data)
     
@@ -50,6 +55,10 @@ def proveedores_ingresar(request):
         form = ProveedorForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request,'Proveedor Ingresado')
+            return redirect('proveedor_ver/')
+        else:
+            messages.error(request,'Error proveedor no ingresado') # Mensaje el cual saldra cuando se meta un Proovedor
     data = {'form': form }
     return render(request,'proveedores_ingresar.html',data)
 
@@ -57,5 +66,13 @@ def proveedores_ver(request):
     proveedor = Proveedor.objects.all()
     data = {'proveedor':proveedor}
     return render(request,'proveedores_ver.html',data)
+
+#def eliminar_proveedor(request, id):
+#    proveedor = get_object_or_404(Proveedor, id = id)
+#    if request.method == 'POST':
+#        proveedor.delete()
+#        messages.success(request,'Proveedor Eliminado')
+#        return redirect('')
+
 
 
